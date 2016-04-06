6b6b3c0c103bba9f10bd7e6b62f3fb645156707b
evaluli <eva.lickert@haufe-lexware.com>
2016-04-06 13:12:13 +0200

    added comments

--- controllers/todo.js
+++ controllers/todo.js
@@ -29,6 +29,7 @@ const findAll = function(req, res, next){
 
 const findOne = function(req, res, next) {
 	const user = req.user;
+	// find the todo-object with ID from route parameters
 	const id = req.params.id;
 
 	if (!id) {
@@ -44,16 +45,21 @@ const findOne = function(req, res, next) {
 
 const updateOne = function(req, res, next){
 	const user = req.user;
+	// find the todo-object with ID from route parameters
 	const id = req.params.id;
 	// const { text, checked } = req.body;
 	const text = req.body.text;
 	const checked = req.body.checked;
 
+	// save an update without any content
 	var update = {};
+	// if there is given text in the request, save it in a new field 'text' in 'update'
 	if (text) { update.text = text; }
+	// if there is new value for 'checked' in the request, save it in a new field 'ckecked' in 'update'
 	if (checked != null) { update.checked = checked; }
 
 	Todo.update({ _id: id, _creator: user._id }, {
+		// replacing value of the mongodb-field with 'update'
 		$set: update
 	}, function(err){
 		if (err) { return next(err); }
@@ -63,6 +69,7 @@ const updateOne = function(req, res, next){
 }
 
 const deleteOne = function(req, res, next) {
+	// find the todo-object with ID from route parameters
 	const id = req.params.id;
 
 	Todo.remove({ _id: id }, function(err){
